2f6574c05c990d296ca9a3eae6a7e7e70704baf1
scastro368 <scastro368@misena.edu.co>
2023-04-28 17:07:39 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById
+    ('root'));
+
+    function holi() {
+       const name = 'path';
+         return  <h1>{name}</h1>
+    }
+root.render(
+     <holi />
+)
